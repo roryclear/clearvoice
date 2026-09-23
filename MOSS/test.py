4e01c7f1153ba19cbf1b99bddba756dd589ca249
Rory Clear <roryclear.rc@gmail.com>
2026-09-23 21:27:17 +0100

    remove stuff

--- MOSS/test.py
+++ MOSS/test.py
@@ -239,67 +239,7 @@ class _BaseAutoModelClass:
     # Base class for auto models.
     _model_mapping = MODEL_FOR_CAUSAL_LM_MAPPING
 
-    def __init__(self, *args, **kwargs) -> None:
-        raise OSError(
-            f"{self.__class__.__name__} is designed to be instantiated "
-            f"using the `{self.__class__.__name__}.from_pretrained(pretrained_model_name_or_path)` or "
-            f"`{self.__class__.__name__}.from_config(config)` methods."
-        )
-
-    @classmethod
-    def from_config(cls, config, **kwargs):
-        trust_remote_code = kwargs.pop("trust_remote_code", None)
-        has_remote_code = hasattr(config, "auto_map") and cls.__name__ in config.auto_map
-        has_local_code = type(config) in cls._model_mapping
-        explicit_local_code = has_local_code and not _get_model_class(
-            config, cls._model_mapping
-        ).__module__.startswith("transformers.")
-        if has_remote_code:
-            class_ref = config.auto_map[cls.__name__]
-            if "--" in class_ref:
-                upstream_repo = class_ref.split("--")[0]
-            else:
-                upstream_repo = None
-            trust_remote_code = resolve_trust_remote_code(
-                trust_remote_code, config._name_or_path, has_local_code, has_remote_code, upstream_repo=upstream_repo
-            )
-
-        if has_remote_code and trust_remote_code and not explicit_local_code:
-            if "--" in class_ref:
-                repo_id, class_ref = class_ref.split("--")
-            else:
-                repo_id = config.name_or_path
-            model_class = get_class_from_dynamic_module(class_ref, repo_id, **kwargs)
-            cls.register(config.__class__, model_class, exist_ok=True)
-            model_class.register_for_auto_class(auto_class=cls)
-            _ = kwargs.pop("code_revision", None)
-            model_class = add_generation_mixin_to_remote_model(model_class)
-            return model_class._from_config(config, **kwargs)
-        elif has_local_code:
-            model_class = _get_model_class(config, cls._model_mapping)
-            text_config_class = config.sub_configs.get("text_config", None)
-            # getattr avoids AttributeError, as registered remote-code model classes may lack config_class
-            if text_config_class is not None and getattr(model_class, "config_class", None) == text_config_class:
-                # TODO: Validate that copying the parent quantization config to the text sub-config preserves
-                # modules_to_not_convert and skip-module matching when composite-model module prefixes differ.
-                parent_config = config
-                config = config.get_text_config()
-                # Check both `quantization_config` being present and also not null,
-                # as a `config.json` can have `"quantization_config": null` in it
-                parent_quant = getattr(parent_config, "quantization_config", None)
-                if parent_quant is not None:
-                    config.quantization_config = parent_quant
-            return model_class._from_config(config, **kwargs)
-
-        raise ValueError(
-            f"Unrecognized configuration class {config.__class__} for this kind of AutoModel: {cls.__name__}.\n"
-            f"Model type should be one of {', '.join(c.__name__ for c in cls._model_mapping)}."
-        )
-
-    @classmethod
-    def _prepare_config_for_auto_class(cls, config: PreTrainedConfig) -> PreTrainedConfig:
-        """Additional autoclass-specific config post-loading manipulation. May be overridden in subclasses."""
-        return config
+    def __init__(self, *args, **kwargs) -> None: pass
 
     @classmethod
     def from_pretrained(cls, pretrained_model_name_or_path: str | os.PathLike[str], *model_args, **kwargs):
